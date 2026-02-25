4d3c8cf87e011b071a5f7addebe43e5843b76e30
iaidris <idrismutolib5@gmail.com>
2026-02-25 14:23:21 +0100

    shiffting debugging to next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,10 @@
 const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
-  }
+  },
+  env: {
+    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
+  },
 }
 
 export default nextConfig
